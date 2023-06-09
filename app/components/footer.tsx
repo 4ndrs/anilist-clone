3d54a856b5b383d81c1a87dd5d9e4d3a7354ad73
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-10 00:48:08 +0200

    Add theme switcher button styles

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,15 +4,31 @@ const Footer = () => (
   <footer className="mt-5 bg-slate-900 p-[1.9rem] text-slate-400">
     <div>
       <h1 className="mb-3 font-bold text-sky-400">Site Theme</h1>
-      <ul className="flex gap-3">
+      <ul className="flex gap-5">
         <li>
-          <button>Default</button>
+          <button
+            aria-label="default"
+            className="relative h-[1.57rem] w-[1.57rem] rounded-[3px] border-[2px] border-slate-500 bg-white text-slate-900"
+          >
+            <span className="absolute left-[0.15rem] top-[0.1rem]">A</span>
+          </button>
         </li>
         <li>
-          <button>Dark</button>
+          <button
+            aria-label="Dark"
+            className="relative h-[1.57rem] w-[1.57rem] rounded-[3px] border-[2px] border-slate-500 bg-slate-900 text-slate-400"
+          >
+            <span className="absolute left-[0.15rem] top-[0.1rem]">A</span>
+          </button>
         </li>
         <li>
-          <button>System Theme</button>
+          <button
+            aria-label="System Theme"
+            className="relative h-[1.57rem] w-[1.57rem] overflow-hidden rounded-[3px] border-[2px] border-slate-500 bg-slate-900 text-slate-400"
+          >
+            <span className="absolute -inset-x-2 inset-y-0 -translate-y-1/3 translate-x-1/4 rotate-45 bg-white" />
+            <span className="absolute left-[0.15rem] top-[0.1rem]">A</span>
+          </button>
         </li>
       </ul>
     </div>
