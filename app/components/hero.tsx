3b1c71b5b673a57269e62245d59ab9bcb7fba58e
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-06-11 18:12:12 +0200

    Adjust font styles

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -35,10 +35,10 @@ const features = [
 
 const Hero = () => (
   <section className="mb-[4.4rem] w-full bg-slate-900 px-10 pb-[1.9rem] pt-[4.4rem] text-slate-100">
-    <h1 className="mb-5 text-center text-[2rem] font-bold leading-snug">
+    <h1 className="mb-5 text-center text-[2rem] font-bold leading-[2.6rem]">
       The next-generation anime platform
     </h1>
-    <p className="mx-auto max-w-[25.13rem] text-center text-[1.32rem] font-medium leading-7 text-blue-300">
+    <p className="mx-auto max-w-[23.39rem] text-center text-[1.312rem] font-medium leading-[1.864rem] text-blue-300">
       Track, share, and discover your favorite anime and manga with AniList.
     </p>
 
@@ -55,7 +55,7 @@ const Hero = () => (
             aria-hidden
             className="row-span-full"
           />
-          <h2 className="mb-2 text-[1.13rem] font-semibold leading-6 text-slate-100">
+          <h2 className="mb-2 text-[1.13rem] font-semibold leading-[1.465rem] text-slate-100">
             {feature.title}
           </h2>
           <p className="text-sm text-blue-300">{feature.description}</p>
